8e38f2f6243d5ec247970352b76698a118b96aaa
Kim Ji Eun <kje6445@gmail.com>
2018-12-10 13:24:12 +0900

    차이를 최대로

--- AlgorithmIntermidiate/src/lesson05/Code_10819.java
+++ AlgorithmIntermidiate/src/lesson05/Code_10819.java
@@ -1,8 +1,10 @@
 package lesson05;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 차이를 최대로 
-
-
 
 문제 
 
@@ -34,9 +36,62 @@ N개의 정수로 이루어진 배열 A가 주어진다. 이때, 배열에 들�
 
 public class Code_10819 {
 
+	public static boolean next_permutation(int[] a) {
+		int i= a.length-1;
+		while(i>0 && a[i-1] >= a[i]) {
+			i -=1;
+		}
+		
+		//마지막 순열
+		if(i <= 0) {
+			return false;
+		}
+		int j = a.length-1;
+		while(a[j] <= a[i-1]) {
+			j -=1;
+		}
+		
+		int temp = a[i-1];
+		a[i-1] = a[j];
+		a[j] = temp;
+		
+		j = a.length-1;
+		while(i<j) {
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i +=1;
+			j -= 1;
+		}
+		return true;
+	}
+	
+	public static int calculate(int a[]) {
+		int sum = 0;
+		for(int i=1; i<a.length; i++) {
+			sum += Math.abs(a[i] - a[i-1]);
+		}
+		return sum;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		Arrays.sort(a);
+		
+		int ans = 0;
+		
+		do {
+			int temp= calculate(a);
+			ans = Math.max(ans, temp);
+		}while(next_permutation(a));
+		
+		System.out.println(ans);
 	}
 
 }
