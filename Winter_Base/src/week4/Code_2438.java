695c4f2eabce821df20721b7a5c579c0db76e939
Kim Ji Eun <ys6445@naver.com>
2019-01-25 23:08:20 +0900

    Q2438(오류 포함),Q2292(오류 포함)

---
+++ Winter_Base/src/week4/Code_2438.java
@@ -0,0 +1,81 @@
+package week4;
+
+import java.util.Scanner;
+
+/*
+별 찍기 - 1 
+
+
+
+
+
+시간 제한
+
+메모리 제한
+
+제출
+
+정답
+
+맞은 사람
+
+정답 비율
+
+
+1 초 128 MB 52020 31396 27721 62.074% 
+
+
+
+
+
+문제
+ 
+
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+
+
+
+입력
+ 
+
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+
+
+
+출력
+ 
+
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+
+
+
+
+
+예제 입력 1 복사 
+ 5
+
+
+
+
+
+예제 출력 1 복사 
+ *
+**
+***
+****
+*****
+
+*/
+public class Code_2438 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int N =sc.nextInt();
+	}
+
+}
