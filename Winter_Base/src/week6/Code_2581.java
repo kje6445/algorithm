47fe05fd078dd444bf1489555dd0ff0534421fe8
Kim Ji Eun <ys6445@naver.com>
2019-02-21 07:53:08 +0900

    Q2581_오류 포함
    소수를 구하는 방식에 대해서 고민을 많이 해볼 것

---
+++ Winter_Base/src/week6/Code_2581.java
@@ -0,0 +1,102 @@
+package week6;
+
+import java.util.Scanner;
+
+/*소수 
+ 
+
+
+문제
+ 
+
+자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
+
+예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
+
+
+
+
+입력
+ 
+
+입력의 첫째 줄에 M이, 둘째 줄에 N이 주어진다.
+
+M과 N은 10,000이하의 자연수이며, M은 N보다 작거나 같다.
+
+
+
+
+출력
+ 
+
+M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다. 
+
+단, M이상 N이하의 자연수 중 소수가 없을 경우는 첫째 줄에 -1을 출력한다.
+
+
+
+
+
+
+예제 입력 1 복사 
+ 60
+100
+
+
+
+
+
+예제 출력 1 복사 
+ 620
+61
+
+
+
+
+
+
+
+예제 입력 2 복사 
+ 64
+65
+
+
+
+
+
+예제 출력 2 복사 
+ -1
+
+*/
+public class Code_2581 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//* 문제 풀이 방식 -> 소수를 먼저 구한뒤 구한 소수를 배열에 입력해 넣고 합을 구할 때는 더하면서 출력, 최소값을 구할 때는 배열의 제일 앞 수를 출력한다.
+		Scanner sc = new Scanner(System.in);
+		
+		int M=sc.nextInt();
+		int N=sc.nextInt();
+		int num=M;
+		int[] primeArr = new int[M-N+1];
+		int sum = 0;
+		
+		while(num>N) {
+			for(int i=2; i<=N; i++) {
+				boolean isPrime = true; //초기 값
+				for(int j=2; j*j<=i; j++) {
+					if(i%j == 0) {
+						//소수가 아니다.
+						isPrime = false;
+						break;
+					}
+				}
+				if(isPrime) {
+					//소수 일 때만 값을 출력
+					primeArr[]
+				}
+			}
+		}
+	}
+
+}
